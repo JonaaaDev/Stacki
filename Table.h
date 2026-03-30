1e7d5bc454f6526981d58c158467186c929eee66
JonaaaDev <jonathangimenezdev@gmail.com>
2026-03-30 04:29:53 +0200

    Create Table.h

---
+++ Table.h
@@ -0,0 +1,32 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "Piece.h"
+#include "raylib.h"
+#include <vector>
+
+class Table {
+private:
+    int width;
+    int height;
+    int cellSize;
+    std::vector<std::vector<Cell>> grid;
+
+public:
+    int score;
+
+    Table(int w = 10, int h = 10, int size = 30);
+
+    bool CanPlace(Piece& piece, int x, int y);
+    void Place(Piece& piece, int x, int y);
+    int CheckLines();
+    bool CanPlaceAny(std::vector<Piece>& pieces);
+    void Draw();
+};
+
+struct Cell {
+    bool filled = false;
+    Color color = RAYWHITE;
+};
+
+#endif
