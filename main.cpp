5b6b6280d6712d2a4f34c6940a87756dc356413c
JonaaaDev <jonathangimenezdev@gmail.com>
2026-03-30 04:31:31 +0200

    Integrate Table class for grid handling in main
    Refactor game logic to use Table class for grid management and drawing.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
 #include "raylib.h"
 #include "Piece.h"
+#include "Table.h"
 #include <vector>
 
-const int GRID_WIDTH = 10;
-const int GRID_HEIGHT = 20;
-const int GRID_SIZE = 30;
-
 int main() {
-    InitWindow(GRID_WIDTH*GRID_SIZE, GRID_HEIGHT*GRID_SIZE, "Stacki Puzzle");
-    SetTargetFPS(60);
-
-    std::vector<std::vector<std::vector<bool>>> grid(GRID_HEIGHT, std::vector<std::vector<bool>>(GRID_WIDTH, std::vector<bool>(1, false)));
+    int cellSize = 40;
+    Table table(10, 10, cellSize);
 
     std::vector<Piece> pieces = {
         Piece({{1,1,1},{0,1,0}}, RED),
@@ -18,7 +13,9 @@ int main() {
         Piece({{1,0},{1,1},{1,0}}, GREEN)
     };
 
-    int score = 0;
+    InitWindow(table.GetWidth()*cellSize, table.GetHeight()*cellSize, "Stacki Puzzle");
+    SetTargetFPS(60);
+
     bool gameOver = false;
 
     while (!WindowShouldClose()) {
@@ -26,36 +23,26 @@ int main() {
         ClearBackground(RAYWHITE);
 
         if (!gameOver) {
-            for (int y = 0; y < GRID_HEIGHT; y++)
-                for (int x = 0; x < GRID_WIDTH; x++)
-                    if (grid[y][x][0])
-                        DrawRectangle(x*GRID_SIZE, y*GRID_SIZE, GRID_SIZE-1, GRID_SIZE-1, BLACK);
+            table.Draw();
 
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                int x = GetMouseX()/GRID_SIZE;
-                int y = GetMouseY()/GRID_SIZE;
+                int x = GetMouseX() / cellSize;
+                int y = GetMouseY() / cellSize;
                 for (auto& p : pieces) {
-                    if (p.CanPlace(grid, x, y)) {
-                        p.Place(grid, x, y);
+                    if (table.CanPlace(p, x, y)) {
+                        table.Place(p, x, y);
                         break;
                     }
                 }
+
+                if (!table.CanPlaceAny(pieces)) gameOver = true;
             }
-
-            bool canPlaceAny = false;
-            for (auto& p : pieces)
-                for (int y = 0; y < GRID_HEIGHT; y++)
-                    for (int x = 0; x < GRID_WIDTH; x++)
-                        if (p.CanPlace(grid, x, y))
-                            canPlaceAny = true;
-
-            if (!canPlaceAny) gameOver = true;
         }
 
-        DrawText(TextFormat("Score: %d", score), 10, 10, 20, BLACK);
+        DrawText(TextFormat("Score: %d", table.score), 10, 10, 20, BLACK);
 
         if (gameOver)
-            DrawText("Game Over", 120, 280, 40, RED);
+            DrawText("Game Over", 120, 200, 40, RED);
 
         EndDrawing();
     }
