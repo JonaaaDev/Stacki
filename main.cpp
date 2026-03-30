17c65191592c0ae907eef0d566d559ac9c915ede
JonaaaDev <jonathangimenezdev@gmail.com>
2026-03-30 04:20:08 +0200

    Initialize window with raylib and main loop

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
-// hola soy jonathan
+#include "raylib.h"
+
+int main() {
+    InitWindow(800, 600, "Stacki");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
